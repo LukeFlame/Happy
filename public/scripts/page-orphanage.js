31cc05ceb99c10dc346d96090709e34991f363dd
LukeFlame <69337083+LukeFlame@users.noreply.github.com>
2020-10-18 20:18:26 -0300

    credits to Leaflet added

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -19,6 +19,7 @@ L.tileLayer(
   "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
   {
     maxZoom: 18,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     id: "mapbox/streets-v11",
     tileSize: 512,
     zoomOffset: -1,
